e5cd253c0a25c072e987b0916178fd9725fb6c8d
Stefan Dausend-Werner <sdausendwerner@hausgross.de>
2017-07-26 08:53:42 +0200

    paths adopted to match src/media/scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
 
 var sassPaths = [
-  'node_modules/normalize.scss/sass',
-  'node_modules/foundation-sites/scss',
-  'node_modules/motion-ui/src'
+  '../normalize.scss/sass',
+  '../foundation-sites/scss',
+  '../motion-ui/src'
 ];
 
 gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
+  return gulp.src('../../src/assets/scss/app.scss')
     .pipe($.sass({
       includePaths: sassPaths,
       outputStyle: 'compressed' // if css compressed **file size**
@@ -17,9 +17,9 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('../../dist/css'));
 });
 
 gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+  gulp.watch(['../../src/assets/scss/**/*.scss'], ['sass']);
 });
